4b24ef798cc9b15b297ddffa43830f6516a004b1
Aryan Chande <aryanchande06@gmail.com>
2026-07-18 18:05:32 +0530

    Binary Tree in C

---
+++ Tree/C/BinaryTree.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int value)
+{
+    struct node *newNode =
+        (struct node *)malloc(sizeof(struct node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void inorder(struct node *root)
+{
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main()
+{
+    struct node *root = createNode(10);
+
+    root->left = createNode(20);
+    root->right = createNode(30);
+
+    root->left->left = createNode(40);
+    root->left->right = createNode(50);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+
+    return 0;
+}
