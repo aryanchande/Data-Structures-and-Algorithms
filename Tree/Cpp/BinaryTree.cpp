90b7cd01ace504ce86632b680204dfb1943f9f5f
Aryan Chande <aryanchande06@gmail.com>
2026-07-18 18:51:08 +0530

    Implemented Binary tree in c++

---
+++ Tree/Cpp/BinaryTree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int value) {
+    if(root == nullptr)
+        return new Node(value);
+
+    if(value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if(root == nullptr)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+
+    inorder(root);
+
+    return 0;
+}
